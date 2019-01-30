e20870144943bf780b9531200dca435a00ee736f
Thinh Nguyen <thanhthinhcntt@gmail.com>
2019-01-30 23:33:23 +0700

    add entity user

---
+++ EStore/EStore/Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EStore.Domain.Entities
+{
+    public class User : Entity<int>,IDateTracking
+    {
+        public string Name { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime? CreatedDate { get; set; } = DateTime.Now;
+
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
